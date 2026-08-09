ab07d1f7f77b963d173c6299d5d8a035e27c3b1b
MUMAIN AHMED <37773524+masumon@users.noreply.github.com>
2026-08-09 23:58:18 +0600

    test(cms): guard public admin-managed setting visibility

---
+++ backend/tests/test_public_settings_visibility.py
@@ -0,0 +1,42 @@
+from app.api.v1.routes.settings import _is_public_setting_key
+
+
+def test_public_cms_image_settings_are_exposed():
+    keys = {
+        "banner_about_image_url",
+        "banner_products_image_url",
+        "banner_blog_image_url",
+        "logo_url",
+        "favicon_url",
+        "app_icon_url",
+        "default_og_image_url",
+        "hero_image_url",
+        "about_story_image_url",
+    }
+    assert all(_is_public_setting_key(key) for key in keys)
+
+
+def test_public_footer_social_and_app_links_are_exposed():
+    keys = {
+        "footer_about_bn",
+        "footer_payment_image_url",
+        "twitter_url",
+        "tiktok_url",
+        "play_store_url",
+        "app_store_url",
+        "contact_address",
+        "contact_email",
+        "seo_title",
+    }
+    assert all(_is_public_setting_key(key) for key in keys)
+
+
+def test_sensitive_settings_remain_private():
+    keys = {
+        "smtp_password",
+        "cloudinary_api_key",
+        "cloudinary_api_secret",
+        "resend_api_key",
+        "secret_key",
+    }
+    assert not any(_is_public_setting_key(key) for key in keys)
